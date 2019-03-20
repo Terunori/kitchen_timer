c0ef73df95ae723efafd0c3fac6cff31f6f1c033
teru <18109251+Terunori@users.noreply.github.com>
2019-03-21 07:05:08 +0900

    set Stats enum

--- kitchen_timer.ino
+++ kitchen_timer.ino
@@ -1,11 +1,35 @@
 #include <M5Stack.h>
 
+enum Stats {
+  Initialized,
+  Moving,
+  Pause,
+  Ended
+};
+
+enum CountModes {
+  up,
+  down
+};
+
+enum Stats stats;
+
 void setup() {
   // put your setup code here, to run once:
-
+  M5.begin();
+  stats = Initialized;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+  if (M5.BtnA.isPressed()) {
+    M5.Lcd.fillScreen(TFT_OLIVE);
+    stats = Moving;
+  }
+
+  if (stats == Moving) {
+    M5.Lcd.printf("hello world");
+  }
+  
 }
