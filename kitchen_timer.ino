2b03b8590837e02b36e890f407f32578dbb75efd
teru <18109251+Terunori@users.noreply.github.com>
2019-03-21 09:14:11 +0900

    set view

--- kitchen_timer.ino
+++ kitchen_timer.ino
@@ -21,6 +21,7 @@ enum Stats stats;
 void setup() {
   // put your setup code here, to run once:
   M5.begin();
+  M5.Lcd.setTextFont(2);
   M5.Lcd.fillScreen(TFT_WHITE);
   stats = Initialized;
   initialize();
@@ -32,30 +33,85 @@ void loop() {
   // put your main code here, to run repeatedly:
   switch (stats) {
     case Initialized:
-      initialize();
+      if (M5.BtnA.wasPressed()) {
+        M5.Lcd.fillRect(0, 0, 320, 180, TFT_WHITE);
+        mm++;
+      } else if (M5.BtnB.wasPressed()) {
+        M5.Lcd.fillRect(0, 0, 320, 180, TFT_WHITE);
+        M5.Lcd.fillScreen(TFT_WHITE);
+        ss++;
+      } else if (M5.BtnC.wasPressed()) {
+        moveTimer();
+      }
       break;
     case Moving:
+      if (M5.BtnC.wasPressed()) {
+        pauseTimer();
+      }
       break;
     case Pause:
+      if (M5.BtnC.wasPressed()) {
+        endTimer();
+      }
       break;
     case Ended:
+      if (M5.BtnC.wasPressed()) {
+        initialize();
+      }
       break;
   }
 }
 
 void initialize() {
-  M5.Lcd.setTextFont(2);
-  M5.Lcd.setTextSize(2);
+  M5.Lcd.clear(TFT_WHITE);
   M5.Lcd.setTextColor(TFT_BLACK);
+  M5.Lcd.setTextSize(2);
   M5.Lcd.setCursor(60, 200);
   M5.Lcd.print("M");
   M5.Lcd.setCursor(150, 200);
   M5.Lcd.print("S");
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.setCursor(240, 200);
+  M5.Lcd.setCursor(215, 200);
   M5.Lcd.print("START");
-  M5.Lcd.setCursor(240, 220);
-  M5.Lcd.print("/STOP");
+  stats = Initialized;
+}
+
+void moveTimer() {
+  M5.Lcd.clear(TFT_LIGHTGREY);
+  M5.Lcd.setTextColor(TFT_WHITE);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(45, 200);
+  M5.Lcd.print("+1M");
+  M5.Lcd.setCursor(120, 200);
+  M5.Lcd.print("RESET");
+  M5.Lcd.setCursor(215, 200);
+  M5.Lcd.print("PAUSE");
+  stats = Moving;
+}
+
+void pauseTimer() {
+  M5.Lcd.clear(TFT_WHITE);
+  M5.Lcd.setTextColor(TFT_BLACK);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(45, 200);
+  M5.Lcd.print("+1M");
+  M5.Lcd.setCursor(120, 200);
+  M5.Lcd.print("RESET");
+  M5.Lcd.setCursor(210, 200);
+  M5.Lcd.print("RESUME");
+  stats = Pause;
+}
+
+void endTimer() {
+  M5.Lcd.clear(TFT_OLIVE);
+  M5.Lcd.setTextColor(TFT_WHITE);
+  M5.Lcd.setTextSize(2);
+  M5.Lcd.setCursor(40, 200);
+  M5.Lcd.print("___");
+  M5.Lcd.setCursor(120, 200);
+  M5.Lcd.print("RESET");
+  M5.Lcd.setCursor(230, 200);
+  M5.Lcd.print("___");
+  stats = Ended;
 }
 
 void printTime() {
